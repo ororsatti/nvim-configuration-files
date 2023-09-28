68d99a2a4fc1e0eebee3c48521864a9911d05eac
Or Antonio Orsatti <antonio@10play.dev>
2023-09-28 22:42:02 +0300

    init

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,12 @@
+return {
+  { 'jose-elias-alvarez/null-ls.nvim' },
+  { 'github/copilot.vim',
+    lazy = false
+  },
+  { 'ggandor/leap.nvim',
+    dependencies = { 'tpope/vim-repeat' },
+  },
+  { 'theprimeagen/harpoon' },
+  { 'mbbill/undotree' }
+}
+
